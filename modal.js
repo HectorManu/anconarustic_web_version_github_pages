5bb25f98d15e17c7575d6e2e0947f261f4fc4bf9
hectorManu <le19081442@itmerida.edu.mx>
2024-02-24 16:31:16 -0600

    update modal.js

--- modal.js
+++ modal.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const galleryContainer = document.getElementById('galleryContainer');
     const imageCount = 56; // Número total de imágenes
+    const excludeList = [55, 52, 49, 47, 48, 44, 38, 17]; // Lista de imágenes para excluir
+
     for (let i = 1; i <= imageCount; i++) {
+        // Verificar si el índice actual debe ser excluido
+        if (excludeList.includes(i)) {
+            continue; // Saltar al siguiente ciclo del bucle si i está en excludeList
+        }
+
         let imgPath = `img/img${i}.webp`; // Asumiendo un patrón de nomenclatura consistente
         let boxDiv = document.createElement('div');
         boxDiv.className = 'box gallery__img';
@@ -11,6 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
         boxDiv.appendChild(imgElement);
         galleryContainer.appendChild(boxDiv);
     }
+
+
 
     // Código para la funcionalidad del modal aquí
     let modal = document.querySelector('#modal');
