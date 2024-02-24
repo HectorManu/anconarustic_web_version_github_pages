60ec532004a6c004845a0427a99d965a3fa3bc9a
hectorManu <le19081442@itmerida.edu.mx>
2024-02-24 16:00:52 -0600

    update code and img

--- modal.js
+++ modal.js
@@ -1,15 +1,31 @@
-let imagenes = document.querySelectorAll('.gallery__img');
-let modal = document.querySelector('#modal');
-let img = document.querySelector('#modal__img');
-let boton = document.querySelector('#modal__boton');
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryContainer = document.getElementById('galleryContainer');
+    const imageCount = 56; // Número total de imágenes
+    for (let i = 1; i <= imageCount; i++) {
+        let imgPath = `img/img${i}.webp`; // Asumiendo un patrón de nomenclatura consistente
+        let boxDiv = document.createElement('div');
+        boxDiv.className = 'box gallery__img';
+        let imgElement = document.createElement('img');
+        imgElement.src = imgPath;
+        imgElement.alt = `Mueble ${i}`;
+        boxDiv.appendChild(imgElement);
+        galleryContainer.appendChild(boxDiv);
+    }
 
-for (let i = 0; i < imagenes.length; i++) {
-    imagenes[i].addEventListener('click', function(e) {
-        modal.classList.toggle("modal--open");
-        let src = e.target.src;
-        img.setAttribute("src", src);
+    // Código para la funcionalidad del modal aquí
+    let modal = document.querySelector('#modal');
+    let img = document.querySelector('#modal__img');
+    let boton = document.querySelector('#modal__boton');
+
+    document.querySelector('.container').addEventListener('click', function(e) {
+        if (e.target && e.target.nodeName === 'IMG') {
+            modal.classList.toggle("modal--open");
+            let src = e.target.src;
+            img.setAttribute("src", src);
+        }
     });
-}
-boton.addEventListener('click',function(){
-    modal.classList.toggle("modal--open");
+
+    boton.addEventListener('click', function() {
+        modal.classList.toggle("modal--open");
+    });
 });
